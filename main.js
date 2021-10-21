5f3560d80404723b143b894cf4ec787b12cd8653
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-21 16:24:51 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+//jQuery 1.6+ use:
+$("#inputID").prop('disabled', true); //disable 
+$("#inputID").prop('disabled', false); //enable
+
+//jQuery 1.5 and below use:
+$("#inputID").attr('disabled','disabled'); //disable 
+$("#inputID").removeAttr('disabled'); //enable
